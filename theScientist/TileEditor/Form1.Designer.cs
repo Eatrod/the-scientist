134301112ffb279c7398f7bb2b46f8469738a128
johan.domander@gmail.com <johan.domander@gmail.com@0e7cd7a5-922b-be4d-811a-0db5f6ae043c>
2014-04-03 09:09:30 +0000

    TileEditorn refactoriserad, BF och JD

--- theScientist/TileEditor/Form1.Designer.cs
+++ theScientist/TileEditor/Form1.Designer.cs
@@ -95,6 +95,8 @@
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.DropDownClosed += new System.EventHandler(this.fileToolStripMenuItem_DropDownClosed);
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
             // 
             // newTileMapToolStripMenuItem
             // 
