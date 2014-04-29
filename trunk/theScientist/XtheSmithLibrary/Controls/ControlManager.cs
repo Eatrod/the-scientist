9d80ae48232dcc76551c52d23402c22720e36e38
be.frost50@gmail.com <be.frost50@gmail.com@0e7cd7a5-922b-be4d-811a-0db5f6ae043c>
2014-04-29 09:41:51 +0000

    Ger nu feedback vid save

--- trunk/theScientist/XtheSmithLibrary/Controls/ControlManager.cs
+++ trunk/theScientist/XtheSmithLibrary/Controls/ControlManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.Concurrent;
 
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -15,8 +16,11 @@ namespace XtheSmithLibrary.Controls
     {
         #region Fields and Properties
 
-        int selectedControl = 0;
+        public int selectedControl = 0;
         static SpriteFont spriteFont;
+        private ConcurrentQueue<Control> removeQueue = new ConcurrentQueue<Control>();
+        private ConcurrentQueue<Control> addQueue = new ConcurrentQueue<Control>();
+        object thisLock = new object();
         public static SpriteFont SpriteFont
         {
             get { return spriteFont; }
@@ -56,12 +60,41 @@ namespace XtheSmithLibrary.Controls
         {
             if (Count == 0)
                 return;
-            foreach (Control c in this)
+            lock (thisLock)
             {
-                if (c.Enabled)
-                    c.Update(gameTime);
-                if (c.HasFocus)
-                    c.HandleInput(playerIndex);
+                foreach (Control c in this)
+                {
+                    if (c.Enabled)
+                        c.Update(gameTime);
+                    if (c.HasFocus)
+                        c.HandleInput(playerIndex);
+                }
+                while (removeQueue.Count() > 0)
+                {
+                    Control c;
+                    bool gotItem = removeQueue.TryDequeue(out c);
+                    if (gotItem)
+                    {
+                        this.Remove(c);
+                    }
+                    
+                }
+                while (addQueue.Count() > 0)
+                {
+                    Control c;
+                    bool gotItem = addQueue.TryDequeue(out c);
+                    if (gotItem)
+                    {
+                        this.Add(c);
+                    }
+                    foreach(Control control in this)
+                    {
+                        control.HasFocus = false;
+                    }
+                    selectedControl = 0;
+                    NextControl();
+                    //c.HasFocus = true;
+                }
             }
             if (InputHandler.KeyPressed(Keys.Up))
                 PreviousControl(); 
@@ -118,5 +151,23 @@ namespace XtheSmithLibrary.Controls
         }
 
         #endregion
+
+        public void RemoveItem(Control item)
+        {
+            removeQueue.Enqueue(item);
+            //lock (thisLock)
+            //{
+            //    this.Remove(item);
+            //}
+        }
+
+        public void AddItem(Control item)
+        {
+            addQueue.Enqueue(item);
+            //lock (thisLock)
+            //{
+            //    this.Add(item);
+            //}
+        }
     }
 }
