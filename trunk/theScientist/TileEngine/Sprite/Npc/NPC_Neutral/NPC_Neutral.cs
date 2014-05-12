18d6b69bc20351f3b80193d880b0f24646cb1af9
amanda.bostrom@gmail.com <amanda.bostrom@gmail.com@0e7cd7a5-922b-be4d-811a-0db5f6ae043c>
2014-05-12 10:54:17 +0000

    MINICOWS / AB

--- trunk/theScientist/TileEngine/Sprite/Npc/NPC_Neutral/NPC_Neutral.cs
+++ trunk/theScientist/TileEngine/Sprite/Npc/NPC_Neutral/NPC_Neutral.cs
@@ -13,16 +13,16 @@ namespace TileEngine.Sprite.Npc.NPC_Neutral
     {
         #region Field Region
         public Conversation text;
-        private const int SpeakingRadius = 40;
+        protected const int SpeakingRadius = 40;
         public bool ShowingBubble { get; set; }
         protected bool startingFlag;
-        private Random random;
-        bool StartingFlag = true;
-        private Vector2 motion;
+        protected Random random;
+        protected bool StartingFlag = true;
+        protected Vector2 motion;
         public bool changeSpeed = true;
 
-        private int direction;
-        private bool collided;
+        protected int direction;
+        protected bool collided;
         protected float delayDirection;
         protected float elapsedDirection;
         protected float WalkingCircle;
@@ -45,33 +45,7 @@ namespace TileEngine.Sprite.Npc.NPC_Neutral
 
         protected NPC_Neutral(Texture2D texture, Script script) : base(texture,script)
         {
-            FrameAnimation down = new FrameAnimation(1, 50, 80, 0, 0);
-            FrameAnimation left = new FrameAnimation(1, 50, 80, 0, 80);
-            FrameAnimation right = new FrameAnimation(1, 50, 80, 0, 160);
-            FrameAnimation up = new FrameAnimation(1, 50, 80, 0, 240);
 
-
-            FrameAnimation walkDown = new FrameAnimation(2, 50, 80, 50, 0);
-            FrameAnimation walkLeft = new FrameAnimation(2, 50, 80, 50, 80);
-            FrameAnimation walkRight = new FrameAnimation(2, 50, 80, 50, 160);
-            FrameAnimation walkUp = new FrameAnimation(2, 50, 80, 50, 240);
-
-
-            this.Animations.Add("Right", right);
-            this.Animations.Add("Left", left);
-            this.Animations.Add("Up", up);
-            this.Animations.Add("Down", down);
-            this.Animations.Add("WalkRight", walkRight);
-            this.Animations.Add("WalkLeft", walkLeft);
-            this.Animations.Add("WalkUp", walkUp);
-            this.Animations.Add("WalkDown", walkDown);
-
-            this.elapsedDirection = 0.0f;
-            this.delayDirection = 10000f;
-            this.WalkingCircle = 500;
-            this.direction = 0;
-            this.random = new Random();
-            this.collided = false;
         }
 
         public override void Update(GameTime gameTime)
--- trunk/theScientist/TileEngine/Sprite/Npc/NPC_Neutral/NPC_Neutral_Townsfolk.cs
+++ trunk/theScientist/TileEngine/Sprite/Npc/NPC_Neutral/NPC_Neutral_Townsfolk.cs
@@ -16,7 +16,40 @@ namespace TileEngine.Sprite.Npc.NPC_Neutral
 
         public NPC_Neutral_Townsfolk(Texture2D texture, Script script) : base(texture, script)
         {
-            
+            FrameAnimation down = new FrameAnimation(1, 50, 80, 0, 0);
+            FrameAnimation left = new FrameAnimation(1, 50, 80, 0, 80);
+            FrameAnimation right = new FrameAnimation(1, 50, 80, 0, 160);
+            FrameAnimation up = new FrameAnimation(1, 50, 80, 0, 240);
+
+
+            FrameAnimation walkDown = new FrameAnimation(2, 50, 80, 50, 0);
+            FrameAnimation walkLeft = new FrameAnimation(2, 50, 80, 50, 80);
+            FrameAnimation walkRight = new FrameAnimation(2, 50, 80, 50, 160);
+            FrameAnimation walkUp = new FrameAnimation(2, 50, 80, 50, 240);
+
+            if (!this.Animations.ContainsKey("Right"))
+                this.Animations.Add("Right", right);
+            if (!this.Animations.ContainsKey("Left"))
+                this.Animations.Add("Left", left);
+            if (!this.Animations.ContainsKey("Up"))
+                this.Animations.Add("Up", up);
+            if (!this.Animations.ContainsKey("Down"))
+                this.Animations.Add("Down", down);
+            if (!this.Animations.ContainsKey("walkRight"))
+                this.Animations.Add("WalkRight", walkRight);
+            if (!this.Animations.ContainsKey("walkLeft"))
+                this.Animations.Add("WalkLeft", walkLeft);
+            if (!this.Animations.ContainsKey("walkUp"))
+                this.Animations.Add("WalkUp", walkUp);
+            if (!this.Animations.ContainsKey("walkDown"))
+                this.Animations.Add("WalkDown", walkDown);
+
+            this.elapsedDirection = 0.0f;
+            this.delayDirection = 10000f;
+            this.WalkingCircle = 500;
+            this.direction = 0;
+            this.random = new Random();
+            this.collided = false;
         }
     }
 }
