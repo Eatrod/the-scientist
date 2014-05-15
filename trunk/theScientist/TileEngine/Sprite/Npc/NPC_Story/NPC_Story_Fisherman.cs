682f90c656f319c13dd4f4af454e6f4fe4186eb9
bjornys88@gmail.com <bjornys88@gmail.com@0e7cd7a5-922b-be4d-811a-0db5f6ae043c>
2014-05-15 22:36:09 +0000

    npc story movement

--- trunk/theScientist/TileEngine/Sprite/Npc/NPC_Story/NPC_Story_Fisherman.cs
+++ trunk/theScientist/TileEngine/Sprite/Npc/NPC_Story/NPC_Story_Fisherman.cs
@@ -13,13 +13,13 @@ namespace TileEngine.Sprite.Npc.NPC_Story
     {
         public NPC_Story_Fisherman(Texture2D texture, Script script, Texture2D picture, string name):base(texture,script,picture,name)
         {
-            FrameAnimation dance = new FrameAnimation(4, 52, 80, 0, 0);
+            FrameAnimation dance = new FrameAnimation(7, 70, 80, 0, 0);
             this.Animations.Add("Dance", dance);
         }
         public override void Update(GameTime gameTime)
         {
             this.CurrentAnimationName = "Dance";
-            this.CurrentAnimation.FramesPerSeconds = 5f;
+            this.CurrentAnimation.FramesPerSeconds = 0.4f;
             base.Update(gameTime);
         }
     }
