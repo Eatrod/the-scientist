f7e2056b97999d50a7e1b1aed0627adf3db89613
bjornys88@gmail.com <bjornys88@gmail.com@0e7cd7a5-922b-be4d-811a-0db5f6ae043c>
2014-05-15 22:55:36 +0000

    npc animation

--- theScientist/TileEngine/Sprite/Npc/NPC_Story/NPC_Story_GuardCaptain.cs
+++ theScientist/TileEngine/Sprite/Npc/NPC_Story/NPC_Story_GuardCaptain.cs
@@ -15,10 +15,11 @@ namespace TileEngine.Sprite.Npc.NPC_Story
         public NPC_Story_GuardCaptain(Texture2D texture, Script script, Texture2D picture, string name)
             : base(texture, script, picture, name)
         {
-            FrameAnimation down = new FrameAnimation(1, 50, 80, 0, 0);
+            FrameAnimation down = new FrameAnimation(9, 50, 80, 150, 0);
             FrameAnimation sleeping = new FrameAnimation(1, 100, 160, 100, 80);
             this.Animations.Add("Sleeping", sleeping);
             this.Animations.Add("Down", down);
+            this.Animations["Down"].FramesPerSeconds = 1f;
         }
     }
 }
