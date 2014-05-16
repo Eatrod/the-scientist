f34ffba0efa3793d58b355c49b1219da3e5f2424
bjornys88@gmail.com <bjornys88@gmail.com@0e7cd7a5-922b-be4d-811a-0db5f6ae043c>
2014-05-16 07:02:01 +0000

    update

--- theScientist/TileGame/TileGame/GameScreens/PotatoTown.cs
+++ theScientist/TileGame/TileGame/GameScreens/PotatoTown.cs
@@ -290,7 +290,7 @@ namespace TileGame.GameScreens
             AnimatedSpriteObject.Add(npcstory);
             NpcStoryList.Add(npcstory);
 
-            npcstory = new NPC_Story(Content.Load<Texture2D>("Sprite/HumanNPCDartagnan"), null, Content.Load<Texture2D>("CharacterPotraits/Anon"), "Johnny");
+            npcstory = new NPC_Story_Johnny(Content.Load<Texture2D>("Sprite/Bjorn_Try_Johnny"), null, Content.Load<Texture2D>("CharacterPotraits/Anon"), "Johnny");
             npcstory.Origionoffset = new Vector2(25, 65);
             npcstory.SetSpritePositionInGameWorld(new Vector2(140, 15));
             AnimatedSpriteObject.Add(npcstory);
